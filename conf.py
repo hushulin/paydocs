40dbfccdfe4bfdd9a34deb873ee3fe847783af70
hushulin <hushulin12321@126.com>
2020-03-03 00:36:17 +0800

    0

--- conf.py
+++ conf.py
@@ -17,19 +17,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx_markdown_tables',
 
-
-]
 
 # -- Project information -----------------------------------------------------
 
@@ -53,6 +41,17 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx_markdown_tables',
+
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
